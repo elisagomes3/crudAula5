94a2d683d418ceb28adb1f07d181fec9a5d95922
Elisa <elisa_m_gomes@estudante.sesisenai.org.br>
2024-09-23 09:33:32 -0300

    Adicionei o delete

--- aula5/read.php
+++ aula5/read.php
@@ -30,5 +30,19 @@ echo "</table>";
 
 $conn -> close();
 }
+
 ?>
-<a href="create.php">Inserir novo registro.</a>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href="create.php">Inserir novo .</a>
+<br>
+<a href="delete.php">Excluir</a>
+    
+</body>
+</html>
+
