694b04acb669b28f69f41cc89e0f2166a3a1a557
Elisa <elisa_m_gomes@estudante.sesisenai.org.br>
2024-09-23 08:40:40 -0300

    read

---
+++ aula5/read.php
@@ -0,0 +1,33 @@
+<?php
+include 'db.php';
+
+$sql = "SELECT * FROM aulas";
+
+$result = $conn -> query($sql);
+
+if($result -> num_rows > 0) {
+    echo"<table border='1'>
+    <tr>
+    <th>ID</th>
+    <th>Sala</th>
+    <th>Local</th>
+    <th>Capacidade</th>
+    </tr>";
+
+    while($row = $result -> fetch_assoc()){
+        echo "<tr>
+        <td>{$row['id_aula']</td>
+        <td>{$row['sala']</td>
+        <td>{$row['local']</td>
+        <td>{$row['capacidade']</td>
+        </td>
+        </tr>";
+    }
+echo "</table>";
+}else{
+    echo "Nenhum registro encontrado.".
+}
+$conn -> close();
+
+?>
+<a href="create.php">Inserir novo registro.</a>
