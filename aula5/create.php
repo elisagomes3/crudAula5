f3ffa9fcf57e679bd4aee548c0380e69096bc7a1
leticia_cisz <leticia_cisz@estudante.sesisenai.org.br>
2024-09-23 09:07:26 -0300

    mudança

---
+++ aula5/create.php
@@ -0,0 +1,32 @@
+<?php
+include 'db.php';
+
+if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
+    $id_aula = $_POST ['id_aula'];
+    $sala= $_POST ['sala'];
+    $local = $_POST ['local'];
+    $capacidade = $_POST ['capacidade'];
+
+
+$sql = "INSERT INTO aulas (id_aula, sala , local , capacidade) VALUES ('$id_aula' , '$sala', '$local', '$capacidade')";
+
+
+if ($conn->query($sql) === true) {
+    echo "Novo registro"; 
+} else{
+    echo "Erro:" . $sql . "<br>" . $conn -> error;
+}
+ 
+$conn -> close();
+}
+?>
+
+<form method="post" action="create.php">
+    Sala: <input type="number" sala="sala" required>
+    Local: <input type="text" local="local" required>
+    Capacidade: <input type="text" capacidade="capacidade" required>
+    <input type="submit" value="Adicionar aula">
+
+</form>
+
+<a href="">Ver Registros das aulas.</a>
---
+++ aula5/update.php
@@ -0,0 +1,19 @@
+<?php
+include 'delete.php';
+
+$id = $_GET['id'];
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $sala = $_POST['sala'];
+    $local = $_POST['local'];
+    $capacidade = $_POST['capacidade'];
+
+    $
+}
+
+
+
+
+
+?>
