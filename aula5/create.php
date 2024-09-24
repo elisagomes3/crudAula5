ee4c3d9a7fbf79f6473ec363997c62925761c091
elisagomes3 <elisa_m_gomes@estudante.sesisenai.org.br>
2024-09-24 09:05:05 -0300

    tirei o id no create

--- aula5/create.php
+++ aula5/create.php
@@ -2,14 +2,13 @@
 include 'db.php';
 
 if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
-    $id_aula = $_POST ['id_aula'] ?? null;
     $sala = $_POST ['sala']?? null;
     $local = $_POST ['local']?? null;
     $capacidade = $_POST ['capacidade']?? null;
     $materia = $_POST ['materia']?? null;
 
 
-$sql = "INSERT INTO aulas (id_aula, sala , local , capacidade, materia) VALUES ('$id_aula' , '$sala', '$local', '$capacidade', '$materia')";
+$sql = "INSERT INTO aulas (sala , local , capacidade, materia) VALUES ('$sala', '$local', '$capacidade', '$materia')";
 
 
 if ($conn->query($sql) === TRUE) {
@@ -23,7 +22,6 @@ $conn -> close();
 ?>
 
 <form method="post" action="create.php">
-    ID Aula: <input type="number" name="id_aula" required><br>
     Sala: <input type="number" name="sala" required><br>
     Local: <input type="text" name="local" required><br>
     Capacidade: <input type="text" name="capacidade" required><br>
